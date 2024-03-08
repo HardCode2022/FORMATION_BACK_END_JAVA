65f553271cf98ed142077fe09927e9a8e0ce8c83
HardCode2022 <formationsoftwaregui@gmail.com>
2024-03-08 14:09:07 +0100

    MGU | AJOUT JSON TEST

--- src/main/java/com/Full_Stack/FormationJavaAngularRestApi/utilisateurs/UtilisateurController.java
+++ src/main/java/com/Full_Stack/FormationJavaAngularRestApi/utilisateurs/UtilisateurController.java
@@ -3,32 +3,24 @@
  */
 package com.Full_Stack.FormationJavaAngularRestApi.utilisateurs;
 
+
+import com.Full_Stack.FormationJavaAngularRestApi.utilisateurs.entite.Utilisateur;
+import com.Full_Stack.FormationJavaAngularRestApi.utilisateurs.exception.PasUtilisateurException;
+import com.Full_Stack.FormationJavaAngularRestApi.utilisateurs.service.UtilisateurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import java.net.URI;
 import java.rmi.ServerException;
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
 
-import com.Full_Stack.FormationJavaAngularRestApi.utilisateurs.service.UtilisateurService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.Full_Stack.FormationJavaAngularRestApi.utilisateurs.entite.Utilisateur;
-import com.Full_Stack.FormationJavaAngularRestApi.utilisateurs.exception.PasUtilisateurException;
-import com.Full_Stack.FormationJavaAngularRestApi.utilisateurs.repoutilisateur.UtilisateurJpaRepo;
-
 /**
- * @author Maurice
+ * @author Maurice G
  *
  */
 @CrossOrigin(origins = "http://localhost:4200")
@@ -91,7 +83,5 @@ public class UtilisateurController {
 		Utilisateur utilisateur = utilisateurService.miseAjourUtilisateur(id,util);
 
 		return new ResponseEntity<Utilisateur>(utilisateur, HttpStatus.OK);
-
 	}
-
 }
